44469b0f3464f8f2e5036caa1590f5f7a74e5329
Abscentee13 <svihaper@gmail.com>
2025-01-20 14:08:02 +0200

    Start 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Language progress 1111");
+        System.out.println("Language progress 2");
     }
 }
